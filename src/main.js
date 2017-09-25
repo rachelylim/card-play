c72e7310abe088e9d15cf581f3e7e4e2611de888
Rachel Lim <rachel.y.lim@gmail.com>
2017-09-24 22:01:15 -0400

    Add react and set up initial app component.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('container')
+  );
+});
