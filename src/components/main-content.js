cf329a3f3a4b3bab3a72e9b9d4dd58814092a441
Rachel Lim <rachel.y.lim@gmail.com>
2017-10-01 21:18:11 -0400

    make sure hover card is still working.

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class MainContent extends React.Component {
+  handleClick() {
+    this.props.onclick && this.props.onclick();
+  }
+
   render() {
     const { header, content } = this.props;
     return (
-      <div className="card-wrapper" onClick={() => this.props.onclick()}>
+      <div className="card-wrapper" onClick={() => this.handleClick()}>
         <p className="card-header">{header}</p>
         <p className="card-content">{content}</p>
       </div>
--- src/components/sub-content.js
+++ src/components/sub-content.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class SubContent extends React.Component {
+  handleClick() {
+    this.props.onclick && this.props.onclick();
+  }
+
   render() {
     const { subcontent } = this.props;
     return (
-      <div className="card-wrapper" onClick={() => this.props.onclick()} >
+      <div className="card-wrapper" onClick={() => this.handleClick()} >
         <p className="card-content">{subcontent}</p>
       </div>
     );
